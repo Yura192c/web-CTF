048824bed297f5adc98c6ea09cf28f7f76ebe0b5
Yura192c <fpm.mironchiYU@bsu.by>
2023-12-30 01:32:17 +0300

    Add BaseUser model Add rehistration/login

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,3 +115,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+LOGIN_REDIRECT_URL = 'leaderboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGOUT_REDIRECT_URL = "login"
--- router.py
+++ router.py
@@ -3,5 +3,5 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('', include('src.index.urls')),
     path('', include('src.task.urls')),
-    path('', include('src.user.urls'))
+    path('user/', include('src.user.urls'))
 ]
